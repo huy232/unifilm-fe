0283506f80223821d4b26130ee5d1575a33675a0
Huy <thai.giahuy123@gmail.com>
2022-08-29 11:16:34 +0700

    Set up for newest page

--- src/components/RecentlyProduct/index.js
+++ src/components/RecentlyProduct/index.js
@@ -1,8 +1,27 @@
-import React from "react"
+import { useState, useEffect } from "react"
+import axios from "axios"
 
 function RecentlyProduct() {
+	const [newestData, setNewestData] = useState([])
+	const newestUrl = "https://unifilm-backend.herokuapp.com/api/update-film"
+
+	useEffect(() => {
+		const CancelToken = axios.CancelToken
+		const source = CancelToken.source()
+
+		axios
+			.get(newestUrl, { cancelToken: source.token })
+			.then((data) => {
+				setNewestData(data)
+			})
+			.catch((thrown) => {
+				if (axios.isCancel(thrown)) return
+			})
+	}, [])
+
 	return (
 		<>
+			{console.log(newestData)}
 			<div className="display">THIS IS RECENTLY PRODUCT PAGE</div>
 		</>
 	)
